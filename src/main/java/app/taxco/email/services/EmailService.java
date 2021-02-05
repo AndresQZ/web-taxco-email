99876279429f9c1cccc6b7b1c246478518f6c570
AndresQZ <landresquiroz@gmail.com>
2021-02-05 13:58:55 -0600

    added signature to email

--- src/main/java/app/taxco/email/services/EmailService.java
+++ src/main/java/app/taxco/email/services/EmailService.java
@@ -28,8 +28,61 @@ public class EmailService {
 	 String message = items.stream().map(item -> item).reduce("", (cadena, item) -> (cadena + "<br>" + item)).toString();
 	 message = "Hola " + userName + "<br> Compra realizada, te compartimos el detalle. <br>" +
 	 "folio: " + folio + "<br>" + message + "<br> Total : $" + amount;
-	 logger.info("message to sendEmail ->  " +  message);
-	 submittedSuccessfully = sendEmailTool(email, message, "Compra realizada");
+	 
+	 String message2 = message +  "<br> <br> <br> <br> <br>" + "<table style=3D\"width:80.0%;border-collapse:collapse\" width=3D\"80%\" cellspa=\r\n"
+	 		+ "cing=3D\"0\" cellpadding=3D\"0\" border=3D\"0\">\r\n"
+	 		+ " <tbody><tr>\r\n"
+	 		+ "  <td style=3D\"padding:0cm 0cm 0cm 0cm\">\r\n"
+	 		+ "  <p class=3D\"MsoNormal\" style=3D\"text-align:center\" align=3D\"center\"><b><s=\r\n"
+	 		+ "pan style=3D\"font-size:15.0pt;font-family:&quot;Arial&quot;,sans-serif;colo=\r\n"
+	 		+ "r:black\">AVISO DE PRIVACIDAD</span></b></p>\r\n"
+	 		+ "  </td>\r\n"
+	 		+ " </tr>\r\n"
+	 		+ " <tr>\r\n"
+	 		+ "  <td style=3D\"padding:0cm 0cm 0cm 0cm\">\r\n"
+	 		+ "  <p class=3D\"MsoNormal\"><span style=3D\"font-size:10.0pt;font-family:&quot;=\r\n"
+	 		+ "Arial&quot;,sans-serif;color:black\">=C2=A0</span></p>\r\n"
+	 		+ "  </td>\r\n"
+	 		+ " </tr>\r\n"
+	 		+ "</tbody></table>\r\n"
+	 		+ "\r\n"
+	 		+ "</div>\r\n"
+	 		+ "\r\n"
+	 		+ "<p class=3D\"MsoNormal\"><span style=3D\"display:none\">=C2=A0</span></p>\r\n"
+	 		+ "\r\n"
+	 		+ "<table style=3D\"width:100.0%;border-collapse:collapse\" width=3D\"100%\" cells=\r\n"
+	 		+ "pacing=3D\"0\" cellpadding=3D\"0\" border=3D\"0\">\r\n"
+	 		+ " <tbody><tr>\r\n"
+	 		+ "  <td style=3D\"border:inset 1.0pt;padding:0cm 0cm 0cm 0cm\">\r\n"
+	 		+ "  <p class=3D\"MsoNormal\"><span style=3D\"font-size:10.0pt;font-family:&quot;=\r\n"
+	 		+ "Arial&quot;,sans-serif;color:black\">MQ JOYERIAS, con\r\n"
+	 		+ "  domicilio en calle Emiliano Zapata #6, colonia Acapantzingo, municipio de=\r\n"
+	 		+ " Cuernavaca,\r\n"
+	 		+ "  C.P. 62440, en la entidad de Morelos, M=C3=A9xico, utilizar=C3=A1 sus dat=\r\n"
+	 		+ "os personales\r\n"
+	 		+ "  recabados para: </span></p>\r\n"
+	 		+ "  <ul type=3D\"disc\">\r\n"
+	 		+ "   <li class=3D\"MsoNormal\" style=3D\"color:black\"><span style=3D\"font-size:1=\r\n"
+	 		+ "0.0pt;font-family:&quot;Arial&quot;,sans-serif\">Dar cumplimento al env=C3=\r\n"
+	 		+ "=ADo de productos seleccionados\r\n"
+	 		+ "       por nuestros clientes en la direcci=C3=B3n proporcionada.</span></li=\r\n"
+	 		+ ">\r\n"
+	 		+ "  </ul>\r\n"
+	 		+ "  <p class=3D\"MsoNormal\" style=3D\"margin-bottom:12.0pt\"><span style=3D\"font=\r\n"
+	 		+ "-size:10.0pt;font-family:&quot;Arial&quot;,sans-serif;color:black\">Para may=\r\n"
+	 		+ "or informaci=C3=B3n acerca del tratamiento y de los derechos\r\n"
+	 		+ "  que puede hacer valer, usted puede acceder al aviso de privacidad integra=\r\n"
+	 		+ "l a\r\n"
+	 		+ "  trav=C3=A9s de: <a href=3D\"http://187.227.238.91:4500\" target=3D\"_blank\">=\r\n"
+	 		+ "http://187.227.238.91:4500</a></span></p>\r\n"
+	 		+ "  </td>\r\n"
+	 		+ " </tr>\r\n"
+	 		+ "</tbody></table></div></span><span><div dir=3D\"ltr\" style=3D\"margin-left:0p=\r\n"
+	 		+ "t\" align=3D\"left\"><span><div dir=3D\"ltr\" style=3D\"margin-left:0pt\" align=3D=\r\n"
+	 		+ "\"left\"></div></span></div></span><span><div dir=3D\"ltr\" style=3D\"margin-lef=\r\n"
+	 		+ "t:0pt\" align=3D\"left\"></div></span></div></div></div></div>";
+	 logger.info("message to sendEmail ->  " +  message2);
+	 submittedSuccessfully = sendEmailTool(email, message2, "Compra realizada");
 	 return submittedSuccessfully;
 		
 	}
@@ -45,10 +98,10 @@ public class EmailService {
 		  logger.info("email to send -> " + email);
 		  logger.info("message to send -> " + textMessage);
 		
-			ClassPathResource image = new ClassPathResource("static/signature.jpg");
+			ClassPathResource logo = new ClassPathResource("static/logo.jpg");
 			helper.setTo(email);
 			helper.setText(textMessage, true);
-			helper.addInline("signature", image);
+			helper.addInline("signature", logo);
 			helper.setSubject(subject);
 			sender.send(message);
 			send = true;
